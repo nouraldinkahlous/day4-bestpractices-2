17060a1a423a34f10e04bde1421190418eff95bb
nouraldinkahlous <nourka@rackham3.uppmax.uu.se>
2021-02-20 21:23:48 +0100

    docs4

--- test_py/docs/conf.py
+++ test_py/docs/conf.py
@@ -29,7 +29,7 @@ release = '20/02/2021'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autodoc',
 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
