7ddb6cd19ee9068dc112d73d375e509e4d8bd88f
nouraldinkahlous <nourka@rackham3.uppmax.uu.se>
2021-02-20 21:16:21 +0100

    docs3

--- test_py/test_simple_math.py
+++ test_py/test_simple_math.py
@@ -1,5 +1,21 @@
 def mathX(n):
-    """  Multiply any number with 10 and adds 10 to it  """
+    """  
+ Multiply any number with 10 and adds 10 to it  
+ 
+ Parameters
+ ----------
+ n : integer
+
+ Returns
+ -------
+ integer
+ 
+ Examples
+ --------
+ >>> mathX(1)
+ 20
+    """
+
     number=n*10+10
     return number
      
